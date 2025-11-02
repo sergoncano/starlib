b2d6c9fef003469b2d7fb7fdda22fd9c87153ecb
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-11-02 23:21:47 +0100

    Added input timeout. Some code was refactored for better readability

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,8 @@ fn game_loop<const MAP_SIZE_X: usize, const MAP_SIZE_Y: usize>(
     print!("{}", planet.generate_banner());
     print!("{}", planet.generate_map());
     loop {
-        let input = get_input();
-        let movement = planet.check_input(&input);
+        let movement_input = get_input();
+        let movement = planet.check_movement_collision(planet.get_player_coords(), movement_input);
         if movement == Movement::Quit {
             restore_terminal_properties();
             std::process::exit(0);
@@ -69,6 +69,7 @@ enum Movement {
     Left,
     Right,
     Quit,
+    Wait,
     Invalid,
 }
 
@@ -116,77 +117,72 @@ impl<const MAP_SIZE_X: usize, const MAP_SIZE_Y: usize> Planet<MAP_SIZE_X, MAP_SI
         return banner;
     }
 
-    fn check_input(&self, input: &String) -> Movement {
-        let input_data = &input[..];
-        match input_data {
-            "w" => {
-                if self.is_valid_movement(&self.player_coords, Movement::Up) {
-                    Movement::Up
-                } else {
-                    Movement::Invalid
-                }
-            }
-            "a" => {
-                if self.is_valid_movement(&self.player_coords, Movement::Left) {
-                    Movement::Left
-                } else {
-                    Movement::Invalid
-                }
-            }
-            "s" => {
-                if self.is_valid_movement(&self.player_coords, Movement::Down) {
-                    Movement::Down
-                } else {
-                    Movement::Invalid
-                }
-            }
-            "d" => {
-                if self.is_valid_movement(&self.player_coords, Movement::Right) {
-                    Movement::Right
-                } else {
-                    Movement::Invalid
-                }
-            }
-            "q" => Movement::Quit,
-            _other => Movement::Invalid,
-        }
-    }
-
     fn move_player(&mut self, movement: Movement) {
         match movement {
             Movement::Up => self.player_coords.y = self.player_coords.y - 1,
             Movement::Left => self.player_coords.x = self.player_coords.x - 1,
             Movement::Down => self.player_coords.y = self.player_coords.y + 1,
             Movement::Right => self.player_coords.x = self.player_coords.x + 1,
-            _other => panic!("Wrong movement in move_player!"),
+            Movement::Wait => (),
+            _other => panic!("Unsupported struct Movement passed to move_player!"),
         }
     }
 
-    fn is_valid_movement(&self, coords: &Coords, movement: Movement) -> bool {
+    //Takes coordinates and a movement and returns the same movement if it is valid. If invalid
+    //returns Movement::Invalid.
+    fn check_movement_collision(&self, coords: &Coords, movement: Movement) -> Movement {
         match movement {
-            Movement::Up => coords.y - 1 >= 0,
-            Movement::Left => coords.x - 1 >= 0,
-            Movement::Down => coords.y + 1 < MAP_SIZE_Y as i32,
-            Movement::Right => coords.x + 1 < MAP_SIZE_X as i32,
-            _other => panic!("Invalid movement sent to is_valid_movement"),
+            Movement::Up => { if coords.y - 1 >= 0 { Movement::Up } else { Movement::Invalid } },
+            Movement::Left => { if coords.x - 1 >= 0 { Movement::Left } else { Movement::Invalid } },
+            Movement::Down => { if coords.y + 1 < MAP_SIZE_Y as i32 { Movement::Down } else { Movement::Invalid } },
+            Movement::Right => { if coords.x + 1 < MAP_SIZE_X as i32 { Movement::Right } else { Movement::Invalid } },
+            Movement::Wait => Movement::Wait,
+            Movement::Invalid => Movement::Invalid,
+            Movement::Quit => Movement::Quit,
         }
+    }
+
+    fn get_player_coords(&self) -> &Coords {
+        &self.player_coords
     }
 }
 
-fn get_input() -> String {
+fn get_input() -> Movement {
     use std::{
         io,
         io::{Read, Write},
     };
+    use timeout_readwrite::TimeoutReader;
+    use std::time::Duration;
     let stdout = io::stdout();
-    let mut reader = io::stdin();
+    let reader = io::stdin();
+    let mut timeout_reader = TimeoutReader::new(reader, Duration::new(3,0));
     let mut buffer = [0; 1];
     stdout.lock().flush().unwrap();
-    reader.read_exact(&mut buffer).unwrap();
-    let result = String::from_utf8(buffer.to_vec());
-    match result {
-        Ok(string) => string,
-        Err(_error) => String::from(""),
+    let result = timeout_reader.read_exact(&mut buffer);
+    let string = match result {
+        Ok(_) => {
+            let key = String::from_utf8(buffer.to_vec());
+            match key {
+                Ok(string) => string,
+                Err(_error) => String::from(""),
+            }
+        },
+        Err(_) => String::from("timeout")
+    };
+    parse_input_text(string)
+}
+
+fn parse_input_text(input: String) -> Movement {
+    let input_data = &input[..];
+    match input_data {
+        "w" => Movement::Up,
+        "a" => Movement::Left,
+        "s" => Movement::Down,
+        "d" => Movement::Right,
+        "q" => Movement::Quit,
+        "timeout" => Movement::Wait,
+        _other => Movement::Invalid,
     }
 }
 
