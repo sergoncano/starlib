c0190c4287589492321af78f257846ef7270f093
sergoncano <sergoncano@noreply-github.com>
2026-01-28 17:26:03 +0100

    Content is now centered in the screen

--- src/util/renderer.rs
+++ src/util/renderer.rs
@@ -2,9 +2,21 @@ use crate::{interfaces::entity::Entity, planet::Planet, util::terminal};
 
 pub fn render_frame(planet: &Planet, entities: &Vec<Box<dyn Entity>>) {
     terminal::clear_screen();
-    print!("{}", planet.generate_banner());
-    print!("{}", planet.generate_map(entities));
+    let (width_u16, height_u16) = crossterm::terminal::size().unwrap_or((0,0));
+    let width = width_u16 as usize;
+    let mut content = format!("{: ^width$}\n", planet.generate_banner().trim());
+    let mut content_height = 0;
+    for line in planet.generate_map(entities).trim().split('\n') {
+        content.push_str(&format!("{: ^width$}", line)[..]);
+        content_height += 1;
+    }
+    for _ in 0..((height_u16 - content_height)/2)-1 {
+        print!("\n");
+    }
+    print!("{}", content);
 }
 pub fn render_tip(tip: String) {
-    println!("{}", tip);
+    let (width_u16, _height_u16) = crossterm::terminal::size().unwrap_or((0,0));
+    let width = width_u16 as usize;
+    println!("{: ^width$}", tip);
 }
