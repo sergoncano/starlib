b339b62c25facb0f333e403dce89c32193cc7caa
sergoncano <sergoncano@noreply-github.com>
2026-01-30 17:23:06 +0100

    Added centered_render function

--- src/util/renderer.rs
+++ src/util/renderer.rs
@@ -1,22 +1,11 @@
-//TODO: Create centered_render function
 use std::cmp;
 
 use crate::{interface::entity::Entity, stage::planet::Planet, util::terminal};
 
 pub fn render_frame(planet: &Planet, entities: &Vec<Box<dyn Entity>>) {
-    terminal::clear_screen();
-    let (width_u16, height_u16) = crossterm::terminal::size().unwrap_or((0, 0));
-    let width = width_u16 as usize;
-    let mut content = format!("{: ^width$}\n", planet.generate_banner().trim());
-    let mut content_height = 0;
-    for line in planet.generate_map(entities).trim().split('\n') {
-        content.push_str(&format!("{: ^width$}", line)[..]);
-        content_height += 1;
-    }
-    for _ in 0..((height_u16 - content_height) / 2) - 1 {
-        println!();
-    }
-    print!("{}", content);
+    let mut content = vec![planet.generate_banner().trim().to_string()];
+    content.extend(planet.generate_map(entities).trim().split('\n').map(|s| s.to_string()));
+    centered_render(content, false);
 }
 pub fn render_tip(tip: String) {
     let (width_u16, _height_u16) = crossterm::terminal::size().unwrap_or((0, 0));
@@ -25,41 +14,34 @@ pub fn render_tip(tip: String) {
 }
 
 pub fn render_menu(header_text: &String, options: &Vec<String>, selected: usize) {
-    terminal::clear_screen();
-    let (width_u16, height_u16) = crossterm::terminal::size().unwrap_or((0, 0));
-    let width = width_u16 as usize;
-    let height = height_u16 as i32;
     let mut max_length: usize = 0;
     for option in options {
         max_length = cmp::max(max_length, option.len());
     }
-    for _ in 0..((height / 2) - ((options.len() as i32 + 1) * 2)) {
-        println!();
-    }
-    print!("{: ^width$}\n\n", header_text);
-    for (i, option) in options
-        .iter()
-        .map(|o| format!("{: <max_length$}", o))
-        .enumerate()
-    {
-        print!(
-            "{: ^width$}",
-            format!("{}{}", if i == selected { "·" } else { " " }, option)
-        );
-        print!("\n\n");
-    }
+    let formatted_options: Vec<String> = options.iter().enumerate().map(|(i, s)| format!("{}{s: <max_length$}", if i==selected { "·" } else { " " })).collect();
+    let mut text: Vec<String> = vec![header_text.clone()];
+    text.extend(formatted_options);
+    centered_render(text, true);
 }
 
 pub fn render_title(title: String, subtitle: Option<String>) {
+    let mut content = vec![title];
+    if subtitle.is_some() {
+        content.push(subtitle.unwrap());
+    }
+    centered_render(content, true);
+}
+
+fn centered_render(text: Vec<String>, separate: bool) {
     terminal::clear_screen();
     let (width_u16, height_u16) = crossterm::terminal::size().unwrap_or((0, 0));
     let width = width_u16 as usize;
-    for _ in 0..(height_u16/2)- if subtitle.is_some() { 2 } else { 1 } {
+    let height = height_u16 as i32;
+    for _ in 0..((height - ((text.len() as i32 + 1) * if separate { 2 } else { 1 })) / 2) {
         println!();
     }
-    println!("{: ^width$}", title);
-    if subtitle.is_some() {
-        println!();
-        println!("{: ^width$}", subtitle.unwrap());
+    for line in text {
+        print!("{: ^width$}\n", line);
+        if separate { println!(); }
     }
 }
